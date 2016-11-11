6024a8181accaa6e9e35cbcea600248c0b2eeaf0
root <root@es-graylog-01.polk.edu>
2016-11-11 21:40:53 +0000

    changes

--- build/graylog2/my_init.d/03_Graylog_Update_Config.sh
+++ build/graylog2/my_init.d/03_Graylog_Update_Config.sh
@@ -10,28 +10,55 @@ if [ ! -f /etc/graylog/server/log4j2.xml ]; then
 fi
 
 # Modify Configuration Files with Environment Variables that were passed
-if [[ ! $(grep -Pzo '####\nCUSTOM SETTINGS\n####' /etc/graylog/server/server.conf) ]]; then echo -e "\n\n################\nCUSTOM SETTINGS\n################\n" >> /etc/graylog/server/server.conf; fi
+#if [[ ! $(grep -Pzo '####\nCUSTOM SETTINGS\n####' /etc/graylog/server/server.conf) ]]; then echo -e "\n\n#### CUSTOM SETTINGS ####\n" >> /etc/graylog/server/server.conf; fi
+
+# Remove Existing Custom Config
+sed -i '/#<-- START SETTINGS -->.*/,$ d' /etc/graylog/server/server.conf
+
+# Start Custom Config Section
+echo -e "#<-- START SETTINGS -->" >> /etc/graylog/server/server.conf
+
 for ENVVARIABLE in ${!GRAYLOG_*}
 do
-	if [[ $(printenv $ENVVARIABLE) ]]; then
-              # Remove Graylog_ Prefix from Environment Variables
-              CONFIGVARIABLE="$(echo ${ENVVARIABLE,,} | sed "s/graylog_//i")"
-              # Get Value for Variable
-              CONFIGVALUE="$(printenv $ENVVARIABLE)"
 
-              # Check if Valid Configuration already exists and skip if so
-              if [[ ! $(egrep "^`printf '%q' ${CONFIGVARIABLE}` = `printf '%q' ${CONFIGVALUE}`" /etc/graylog/server/server.conf) ]]; then
+	# Remove Graylog_ Prefix from Environment Variables
+        CONFIGVARIABLE="$(echo ${ENVVARIABLE,,} | sed "s/graylog_//i")"
+        # Get Value for Variable
+        CONFIGVALUE="$(printenv $ENVVARIABLE)"
 
-                  # Check If Config Variable is in configuration and not commented out.
-                  if [[ $(egrep "^${CONFIGVARIABLE}" /etc/graylog/server/server.conf) ]]; then
-                      # Comment it out so it can be added to the end of the file
-                      sed -i "s|^${CONFIGVARIABLE}|#${CONFIGVARIABLE}|g" /etc/graylog/server/server.conf
-                      echo "${CONFIGVARIABLE} = ${CONFIGVALUE}" >> /etc/graylog/server/server.conf
-                  else
-                      # Wasn't uncommented in config so adding to end of configuration file
-                      echo "${CONFIGVARIABLE} = ${CONFIGVALUE}" >> /etc/graylog/server/server.conf
-                  fi
+        # Check If Config Variable is in configuration and not commented out.
+        if [[ $(egrep "^${CONFIGVARIABLE}" /etc/graylog/server/server.conf) ]]; then
+ 	       # Comment it out so it can be added to the end of the file
+               sed -i "s|^${CONFIGVARIABLE}|#${CONFIGVARIABLE}|g" /etc/graylog/server/server.conf
+	fi
 
-              fi
-        fi
+	# Write Variable
+	echo "${CONFIGVARIABLE} = ${CONFIGVALUE}" >> /etc/graylog/server/server.conf
+
+
+#	if [[ $(printenv $ENVVARIABLE) ]]; then
+#              # Remove Graylog_ Prefix from Environment Variables
+#              CONFIGVARIABLE="$(echo ${ENVVARIABLE,,} | sed "s/graylog_//i")"
+#              # Get Value for Variable
+#              CONFIGVALUE="$(printenv $ENVVARIABLE)"
+#
+#              # Check if Valid Configuration already exists and skip if so
+#              if [[ ! $(egrep "^`printf '%q' ${CONFIGVARIABLE}` = `printf '%q' ${CONFIGVALUE}`" /etc/graylog/server/server.conf) ]]; then
+#
+#                  # Check If Config Variable is in configuration and not commented out.
+#                  if [[ $(egrep "^${CONFIGVARIABLE}" /etc/graylog/server/server.conf) ]]; then
+#                      # Comment it out so it can be added to the end of the file
+#                      sed -i "s|^${CONFIGVARIABLE}|#${CONFIGVARIABLE}|g" /etc/graylog/server/server.conf
+#                      echo "${CONFIGVARIABLE} = ${CONFIGVALUE}" >> /etc/graylog/server/server.conf
+#                  else
+#                      # Wasn't uncommented in config so adding to end of configuration file
+#                      echo "${CONFIGVARIABLE} = ${CONFIGVALUE}" >> /etc/graylog/server/server.conf
+#                  fi
+#
+#              fi
+#        fi
+
 done
+
+# End Custom Config Section
+echo -e "#<-- END SETTINGS -->" >> /etc/graylog/server/server.conf
