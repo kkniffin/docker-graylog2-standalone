3dca69fae38a6d43dbe86549fcae123eb70cf6d7
root <root@es-graylog-01.polk.edu>
2016-11-15 18:51:35 +0000

    changes

--- build/mongodb/files/scripts/setup_mongodb.sh
+++ build/mongodb/files/scripts/setup_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # If MongoDB is Primary then setup User's and Database
-if [[ ${MONGODB_REPLICATIONSET_PRIMARY,,} = "true" ]] && [ ! -f /data/db/db_is_setup ]; then
+if [[ ${MONGODB_REPLICATIONSET_PRIMARY,,} = "true" ]] && [ ! -f /data/db/db_is_setup ] && [ ${MONGODB_REPLICATIONSET_HOSTNAME+"false"} ]; then
 
 # Wait for MongoDB to come up.
 echo "###################################################################################"
@@ -10,41 +10,69 @@ echo "##########################################################################
 sleep 10
 
 # Initiate Replica Node as Primary
-echo "###################################################################################"
-echo "####### MONGO SETUP - NEEDS TO BE SETUP AS PRIMARY ################################"
-echo "###################################################################################"
-mongo --eval "var res = rs.initiate(); printjson(res)"
+#echo "###################################################################################"
+#echo "####### MONGO SETUP - NEEDS TO BE SETUP AS PRIMARY ################################"
+#echo "###################################################################################"
+#mongo --eval "var res = rs.initiate(); printjson(res)"
 
 # Wait for Replication to Complete
-echo "###################################################################################"
-echo "####### MONGO SETUP - SLEEPNG FOR 5 SECS FOR REPLICATION TO FINISH ################"
-echo "###################################################################################"
-sleep 5
+#echo "###################################################################################"
+#echo "####### MONGO SETUP - SLEEPNG FOR 5 SECS FOR REPLICATION TO FINISH ################"
+#echo "###################################################################################"
+#sleep 5
 
-	# Export Config to js file for reading into Mongo
+# Post Hostname to Local Hosts File, otherwise wont come up.
+echo "127.0.0.1 ${MONGODB_REPLICATIONSET_HOSTNAME}" >> /etc/hosts
+
+# Export Config to js file for reading into Mongo
 cat <<EOT> /tmp/setup_mongodb.js
+
+var replicaconfig = { _id: "${MONGODB_REPLICATIONSET_NAME}",
+    members: [
+        { _id: 0, host: "${MONGODB_REPLICATIONSET_HOSTNAME}"}
+    ]
+};
+print('################ MONGODB - Initiating Replica Configuration ################');
+rs.initiate(replicaconfig);
+
+print('################ MONGODB - Checking Replica Status ################');
+while ( rs.status().startupStatus || (rs.status().hasOwnProperty("myState") && rs.status().myState != 1)) {
+	print('################ MONGODB - Replica Not Ready, Status is: ################');
+	printjson( rs.status() );
+	sleep(1000);
+};
+
+print('################ MONGODB - Replica Ready, Status is: ################');
+printjson( rs.status() );
+
 admin = db.getSiblingDB("admin")
+
+print('################ MONGODB - Creating SuperAdmin ################');
 db.getSiblingDB("admin").createUser(
-          {
-            "user" : "superadmin",
-            "pwd" : "${MONGODB_SUPERADMIN_PW}",
-            roles: [ { "role" : "root", "db" : "admin" } ]
-          }
-       )
+  {
+    "user" : "superadmin",
+    "pwd" : "${MONGODB_SUPERADMIN_PW}",
+    roles: [ { "role" : "root", "db" : "admin" } ]
+  }
+)
 
-       db.getSiblingDB("admin").auth("superadmin", "${MONGODB_SUPERADMIN_PW}" )
+print('################ MONGODB - Authenticating as SuperAdmin ################');
+db.getSiblingDB("admin").auth("superadmin", "${MONGODB_SUPERADMIN_PW}" )
 
-       use graylog
-       db.createUser(
-         {
-           user: "graylogdb",
-           pwd:"${MONGODB_GRAYLOGDB_PW}",
-	   roles: [
-                    { role: "readWrite", db:"graylog" },
-                    { role: "dbAdmin", db:"graylog" }
-                  ]
-         }
-       )
+print('################ MONGODB - Creating Graylog DB ################');
+use graylog
+
+print('################ MONGODB - Creating Graylog User ################');
+db.createUser(
+  {
+    user: "graylogdb",
+    pwd:"${MONGODB_GRAYLOGDB_PW}",
+    roles: [
+      { role: "readWrite", db:"graylog" },
+      { role: "dbAdmin", db:"graylog" }
+    ]
+  }
+)
 EOT
 
 echo "###################################################################################"
@@ -75,4 +103,3 @@ fi
 #            roles: [ { role: "userAdminAnyDatabase", "db" : "admin" } ]
 #          }
 #       )
-
